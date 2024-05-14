c85261f52e5a65564a560e5b15071230d9ec7207
Ramon Vicente <ramon.silva.1396106@sga.pucminas.br>
2024-05-14 15:01:47 -0300

    Adiciona relacionamento

--- src/api/models.py
+++ src/api/models.py
@@ -1,9 +1,10 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-# Database model
+# Database item model
 class Item(Base):
 	__tablename__ = "items"
 	id = Column(Integer, primary_key=True, index=True)
@@ -11,7 +12,7 @@ class Item(Base):
 	description = Column(String)
 
 
-# Database model
+# Database institutions model
 class Institution(Base):
 	__tablename__ = "institutions"
 	id = Column(Integer, primary_key=True, index=True)
@@ -19,13 +20,13 @@ class Institution(Base):
 	address = Column(String)
 	latitude = Column(String)
 	longitude = Column(String)
-	responsibleUserId = Column(Integer) # Todo: consertar relacionamento
+	responsibleUserId = Column(Integer, ForeignKey('users.id')) # Adiciona relacionamento
 	created = Column(String)
 	visitTime = Column(String)
 	visitDates = Column(String)
 
 
-# Database model
+# Database user model
 class User(Base):
 	__tablename__ = "users"
 	id = Column(Integer, primary_key=True, index=True)
@@ -34,39 +35,39 @@ class User(Base):
 	socialUid = Column(String)
 
 
-# Database model
+# Database comment  model
 class Comment(Base):
 	__tablename__ = "comments"
 	id = Column(Integer, primary_key=True, index=True)
-	userId = Column(Integer) # Todo: consertar relacionamento
+	userId = userId = Column(Integer, ForeignKey('users.id')) # Adiciona relacionamento
 	comment = Column(String)
-	postId = Column(Integer) # Todo: consertar relacionamento
+	postId = Column(Integer, ForeignKey('posts.id')) # Adiciona relacionamento
 	timeStamp = Column(String)
 	
-# Database model
+# Database posts model
 class Post(Base):
 	__tablename__ = "posts"
 	id = Column(Integer, primary_key=True, index=True)
-	userId = Column(Integer) # Todo: consertar relacionamento
-	institutionId = Column(Integer) # Todo: consertar relacionamento
+	userId = userId = Column(Integer, ForeignKey('users.id')) # Adiciona relacionamento
+	institutionId = institutionId = Column(Integer, ForeignKey('institutions.id')) # Adiciona relacionamento
 	text = Column(String)
 	timeStamp = Column(String)
 
 
-# Database model
+# Database PostLikes model
 class PostLikes(Base):
 	__tablename__ = "postLikes"
 	id = Column(Integer, primary_key=True, index=True)
-	userId = Column(Integer) # Todo: consertar relacionamento
-	postId = Column(Integer) # Todo: consertar relacionamento
+	userId = userId = Column(Integer, ForeignKey('users.id')) # Adiciona relacionamento
+	postId = postId = Column(Integer, ForeignKey('posts.id')) # Adiciona relacionamento
 	timeStamp = Column(String)
 
 
-# Database model
+# Database UserFavorites model
 class UserFavorites(Base):
 	__tablename__ = "userFavorites"
 	id = Column(Integer, primary_key=True, index=True)
-	userId = Column(Integer) # Todo: consertar relacionamento
-	institutionId = Column(Integer) # Todo: consertar relacionamento
+	userId = userId = Column(Integer, ForeignKey('users.id')) # Adiciona relacionamento
+	institutionId = institutionId = Column(Integer, ForeignKey('institutions.id')) # Adiciona relacionamento
 
 
