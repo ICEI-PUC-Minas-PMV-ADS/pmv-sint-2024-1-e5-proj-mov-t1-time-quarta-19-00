6d5fb6857df2ace196a23491adb9005f3d7be08c
Paulo Henrique Vieira Candido <phvcandido@gmail.com>
2024-06-19 20:11:15 -0300

    Adiciona novas telas

---
+++ src/app/app/post/[slug].tsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+type Props = {};
+
+const Post = (props: Props) => {
+  return (
+    <View>
+      <Text>Post</Text>
+    </View>
+  );
+};
+
+export default Post;
---
+++ src/app/app/registerInstitution.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+type Props = {};
+
+const RegisterInstitution = (props: Props) => {
+  return (
+    <View>
+      <Text>RegisterInstitution</Text>
+    </View>
+  );
+};
+
+export default RegisterInstitution;
