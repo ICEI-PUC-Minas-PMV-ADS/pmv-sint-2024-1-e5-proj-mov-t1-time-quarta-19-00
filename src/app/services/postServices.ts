56e58288e00ad46b964f63b56be81dca5cc8920c
Paulo Henrique Vieira Candido <phvcandido@gmail.com>
2024-06-19 21:07:03 -0300

    Adiciona serviços de api do post

---
+++ src/app/services/postServices.ts
@@ -0,0 +1,11 @@
+import { endpoints } from "@/config/endpoints";
+import { ApiServices } from "./apiServices";
+
+export const postServices = {
+  createPost: async (data: any) => {
+    return (await ApiServices.create(endpoints.posts, data)).data;
+  },
+  fetchPosts: async () => {
+    return (await ApiServices.search(endpoints.posts)).data.reverse();
+  },
+};
