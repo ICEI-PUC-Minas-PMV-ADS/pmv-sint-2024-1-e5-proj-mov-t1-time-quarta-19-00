742369b14d724a10ab9e91f1f0416ee6fd7bcd8c
Paulo Henrique Vieira Candido <phvcandido@gmail.com>
2024-06-21 20:34:33 -0300

    Adiciona lista de comentários do post

--- src/app/app/comments/[slug].tsx
+++ src/app/app/comments/[slug].tsx
@@ -1,14 +1,46 @@
-import { View, Text } from "react-native";
+import { ScrollView, Text, SafeAreaView, StyleSheet } from "react-native";
 import React from "react";
+import { Appbar } from "react-native-paper";
+import { router, useLocalSearchParams } from "expo-router";
+import usePosts from "@/dataHooks/usePosts";
+import { PostDTO } from "../feed";
 
 type Props = {};
 
 const PostComments = (props: Props) => {
+  const { slug } = useLocalSearchParams();
+  const { data } = usePosts(+(slug || 0) || null);
+  const castData = data as PostDTO;
+
+  const goToPosts = () => {
+    router.replace("/feed");
+  };
+
   return (
-    <View>
-      <Text>PostComments</Text>
-    </View>
+    <SafeAreaView style={styles.containerScrollArea}>
+      <Appbar.Header>
+        <Appbar.BackAction onPress={goToPosts} />
+        <Appbar.Content title={`Comentários do post ${castData?.id ?? ""}`} />
+      </Appbar.Header>
+      <ScrollView style={styles.containerPostText}>
+        <Text>PostComments</Text>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  postImage: {
+    width: "100%",
+    height: 400,
+  },
+  containerPostText: {
+    padding: 16,
+  },
+  containerScrollArea: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
 
 export default PostComments;
