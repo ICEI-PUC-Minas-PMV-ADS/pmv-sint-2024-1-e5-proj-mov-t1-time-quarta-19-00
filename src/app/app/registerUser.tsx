75c37a4263b221ab59efc0e1f6c242eb0fceabab
Paulo Henrique Vieira Candido <phvcandido@gmail.com>
2024-06-22 08:32:58 -0300

    Integração inicial do cadastro de usuário

--- src/app/app/registerUser.tsx
+++ src/app/app/registerUser.tsx
@@ -2,24 +2,67 @@ import { StyleSheet, View } from "react-native";
 import React, { useState } from "react";
 import { Appbar, Button, Text, TextInput } from "react-native-paper";
 import { router } from "expo-router";
+import { createUserMutation } from "@/dataHooks/useUsers";
+import { User } from "@/services";
+import ErrorMessage from "@/components/ErrorMessage";
 
 type Props = {};
 
-interface UserRegisterData {
-  email: string;
-  password: string;
-  name: string;
-}
+type UserRegisterData = User;
 
 const RegisterUser = (props: Props) => {
   const [userData, setUserData] = useState({} as UserRegisterData);
+  const [error, setError] = useState("");
 
   const handleData = (key: keyof UserRegisterData, value: string) => {
     setUserData({ ...userData, [key]: value });
   };
 
+  /**
+   * Verifica se todos os itens não estão vazios no userData,
+   * caso esteja, retorna um erro
+   */
+  const validate = () => {
+    // se o objeto estiver vazio, retorna um erro
+    if (Object.keys(userData).length === 0) {
+      setError("Preencha todos os dados");
+      return false;
+    }
+
+    // estrutura do objeto fim
+    if (
+      userData?.email === "" ||
+      userData?.password === "" ||
+      userData?.name === "" ||
+      userData?.username === ""
+    ) {
+      setError("Preencha todos os dados");
+    }
+
+    if (Object.values(userData).some((value) => value === "")) {
+      setError("Preencha todos os dados");
+      return false;
+    }
+
+    return true;
+  };
+
+  const { mutateAsync, isPending: isIdle } = createUserMutation();
+
+  const submitData = async () => {
+    try {
+      await mutateAsync(userData);
+      router.replace("/login");
+    } catch (err) {
+      setError("Erro ao cadastrar usuário");
+    }
+  };
+
   const doRegister = () => {
-    console.log("Do Register");
+    if (!validate()) {
+      return;
+    }
+    submitData();
   };
 
   const doRegisterInstitution = () => {
@@ -40,25 +83,36 @@ const RegisterUser = (props: Props) => {
         <Text variant="titleMedium">
           Preencha seus dados para fazer seu cadastro
         </Text>
+        <ErrorMessage error={error} />
         <View style={style.containerForm}>
           <TextInput
+            disabled={isIdle}
             style={style.submitButton}
             label="Name"
             value={userData.name}
             onChangeText={(text) => handleData("name", text)}
           />
           <TextInput
+            disabled={isIdle}
             label="Email"
             value={userData.email}
             onChangeText={(text) => handleData("email", text)}
           />
           <TextInput
+            disabled={isIdle}
+            label="Nome de Usuário"
+            value={userData.username}
+            onChangeText={(text) => handleData("username", text)}
+          />
+          <TextInput
+            disabled={isIdle}
             label="Password"
             secureTextEntry
             value={userData.password}
             onChangeText={(text) => handleData("password", text)}
           />
           <Button
+            disabled={isIdle}
             style={style.submitButton}
             icon="login"
             mode="contained"
--- src/app/services/userServices.ts
+++ src/app/services/userServices.ts
@@ -1,14 +1,22 @@
 import { endpoints } from "@/config/endpoints";
 import { ApiServices } from "./apiServices";
 
+export interface User {
+  id?: number;
+  name: string;
+  username: string;
+  email: string;
+  password: string;
+}
+
 export const userServices = {
-  createUser: async (data: any) => {
+  createUser: async (data: User) => {
     return (await ApiServices.create(endpoints.users, data)).data;
   },
-  fetchUsers: async () => {
+  fetchUsers: async (): Promise<User[]> => {
     return (await ApiServices.search(endpoints.users)).data.reverse();
   },
-  getUserById: async (userId: string) => {
+  getUserById: async (userId: string): Promise<User> => {
     return (await ApiServices.search(`${endpoints.users}/${userId}`)).data;
   },
 };
